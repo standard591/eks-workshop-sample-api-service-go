bca7337f92ee4f2172e28dbb0fc47ee070a85a72
standard591 <59709783+standard591@users.noreply.github.com>
2020-02-11 15:48:46 -0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Say Hello To My Little Friend!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
